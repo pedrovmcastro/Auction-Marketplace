59296e68a1d604b8bbf2a1d701051666d9077529
pedrovmcastro <pvmcastro@gmail.com>
2024-10-08 11:01:02 -0300

    implemented Category and AuctionListing models

--- auctions/models.py
+++ auctions/models.py
@@ -6,9 +6,25 @@ class User(AbstractUser):
     pass
 
 
-class AuctionListing(models.Model):
-    pass
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
+class AuctionListing(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    photo = models.ImageField(upload_to='listings/', null=True, blank=True)
+    datetime_submited = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    listed_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
 
 class Bid(models.Model):
     pass
