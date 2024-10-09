718d6a856d2d5b662e30ed876448735c7867d7d6
pedrovmcastro <pvmcastro@gmail.com>
2024-10-09 16:29:38 -0300

    update Category model: added img field

--- auctions/models.py
+++ auctions/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='categories/', null=True, blank=True)
 
     def __str__(self):
         return self.name
