f371800b4af3c2e4bed4fbcde718edeadfb29207
pedrovmcastro <pvmcastro@gmail.com>
2024-10-09 08:36:25 -0300

    renamed 'price' to 'current_bid'

--- auctions/models.py
+++ auctions/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class AuctionListing(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2) 
     photo = models.ImageField(upload_to='listings/', null=True, blank=True)
     datetime_submited = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
