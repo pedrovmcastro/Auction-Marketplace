49e34780fdb9cfb0c90236db8233e49f0369f58b
pedrovmcastro <pvmcastro@gmail.com>
2024-10-09 16:28:56 -0300

    created path 'categories' and settings.DEBUG to render imgs

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,5 +9,9 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("create", views.create, name="create")
+    #path("create", views.create, name="create")
+    path("categories", views.categories, name="categories")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
