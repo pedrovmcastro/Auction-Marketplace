3f1ba8f7477a8e27bccfe010a07bd97b6301a2d4
pedrovmcastro <pvmcastro@gmail.com>
2024-10-10 21:23:02 -0300

    added categories, categories_matches, listing and create paths

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("categories", views.categories, name="categories")
-    #path("create", views.create, name="create")
+    path("categories", views.categories, name="categories"),
+    path("categories/<int:category_id>", views.category_matches, name="category_matches"),
+    path("listing/<int:listing_id>", views.listing_details, name="listing_details"),
+    path("create", views.create, name="create")
 ]
 
 if settings.DEBUG:
