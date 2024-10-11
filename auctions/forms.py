3ad71fd092cf35d9a722991ea4b894b55e41ae07
pedrovmcastro <pvmcastro@gmail.com>
2024-10-10 21:21:31 -0300

    update CreateListingForm: form -> modelform

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 
+from .models import AuctionListing
+
+"""
 class CreateListingForm(forms.Form):
     name = forms.CharField(label="Name:", required=True)
     description = forms.CharField(label="Description:", widget=forms.Textarea(), required=False)
     starting_bid = forms.DecimalField(label="Starting Bid:", required=True)
     photo = forms.ImageField(label="Photo:", required=False)
+"""
 
+class CreateListingForm(forms.ModelForm):
+    class Meta:
+        model = AuctionListing
+        fields = ['name', 'description', 'current_bid', 'photo', 'category']
