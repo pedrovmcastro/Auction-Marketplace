96117d9fb33bb3e4922a030eb27f982f02b9a69d
pedrovmcastro <pvmcastro@gmail.com>
2024-10-14 08:45:01 -0300

    normalization

--- auctions/forms.py
+++ auctions/forms.py
@@ -2,13 +2,6 @@ from django import forms
 
 from .models import AuctionListing, Comment, Bid
 
-"""
-class CreateListingForm(forms.Form):
-    name = forms.CharField(label="Name:", required=True)
-    description = forms.CharField(label="Description:", widget=forms.Textarea(), required=False)
-    starting_bid = forms.DecimalField(label="Starting Bid:", required=True)
-    photo = forms.ImageField(label="Photo:", required=False)
-"""
 
 class CreateListingForm(forms.ModelForm):
     class Meta:
